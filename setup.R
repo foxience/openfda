d9809af6e26da9d0a82402deeabfa7ef1b73f554
Foxience <foxience@foxience.delivn>
2016-11-20 23:39:17 +0800

    add script to setup and install necessary packages

---
+++ setup.R
@@ -0,0 +1,6 @@
+# The full tutorial can be found here: https://github.com/rOpenHealth/openfda
+
+install.packages('devtools')
+library(devtools)
+devtools::install_github('ropenhealth/openfda')
+library(openfda)
