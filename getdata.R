d68b0b832e7feee2839e75694ed33dc23a0fac19
Foxience <foxience@foxience.delivn>
2016-11-20 21:20:02 +0800

    modify script to get data from openfda

--- getdata.R
+++ getdata.R
@@ -1,6 +1,6 @@
 library(openfda)
-fda_query('/drug/event.json') %>%
-  # fda_filter('occurcountry', 'usa') %>%
-  # fda_search() %>% fda_limit() %>%
-  fda_count('occurcountry') %>%
+df <- fda_query('/drug/event.json') %>%
+  fda_filter('occurcountry', 'vn') %>%
+  fda_search() %>% fda_limit(20) %>%
+  # fda_count('occurcountry') %>%
   fda_exec()
