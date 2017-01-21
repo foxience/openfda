03d4ef3e8facb4b9938388274f48254c9c7737eb
Foxience <foxience@foxience.delivn>
2017-01-22 03:42:48 +0800

    update setup script

--- getdata.R
+++ getdata.R
@@ -1,6 +1,15 @@
+library(tidyr)
+library(dplyr)
+library(ggplot2)
 library(openfda)
+
 df <- fda_query('/drug/event.json') %>%
-  fda_filter('occurcountry', 'vn') %>%
-  fda_search() %>% fda_limit(20) %>%
-  # fda_count('occurcountry') %>%
+  fda_filter('patient.patientsex', 0) %>%
+  fda_filter('patient.patientonsetage', '[25+TO+40]') %>%
+  fda_filter('patient.drug.medicinalproduct', 'amitriptylin') %>%
+  # fda_filter('patient.drug.drugindication', 'PYELONEPHRITIS') %>%
+  # fda_filter('occurcountry', 'vn') %>%
+  # fda_search() %>% fda_limit(20) %>%
+  fda_count('patient.reaction.reactionmeddrapt.exact') %>%
+  # fda_count('patient.patientonsetage') %>%
   fda_exec()
--- setup.R
+++ setup.R
@@ -4,3 +4,7 @@ install.packages('devtools')
 library(devtools)
 devtools::install_github('ropenhealth/openfda')
 library(openfda)
+
+# Install mxnet for full-featured neural network modeling
+install.packages('/home/mxnet/mxnet_0.7.tar.gz', repos = NULL, type="source")
+library(mxnet)
