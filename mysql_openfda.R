bb0e5a61b2b439c41d1044d8685a6beff962dc17
Foxience <foxience@foxience.delivn>
2017-02-12 19:14:54 +0800

    Add query for drug and substance

--- mysql_openfda.R
+++ mysql_openfda.R
@@ -20,5 +20,16 @@ reports <- dbGetQuery(con, "SELECT * FROM `report` limit 10")
 print(reports)
 View(reports)
 
-report <- dbGetQuery(con, "SELECT * FROM `report` WHERE id='00000010-ec8e-11e6-aaaa-70b2e7692ca3'")
+report <- dbGetQuery(con, "SELECT * FROM `report` WHERE id = '00000010-ec8e-11e6-aaaa-70b2e7692ca3'")
 View(report)
+patient <- dbGetQuery(con, "SELECT * FROM `patient` WHERE reportId = '00000010-ec8e-11e6-aaaa-70b2e7692ca3'")
+View(patient)
+reaction <- dbGetQuery(con, "SELECT * FROM `reaction` WHERE reportId = '00000010-ec8e-11e6-aaaa-70b2e7692ca3'")
+View(reaction)
+drug <- dbGetQuery(con, "SELECT * FROM `drug` WHERE reportId = '00000010-ec8e-11e6-aaaa-70b2e7692ca3'")
+View(drug)
+
+substanceQuery <- paste("SELECT * FROM `substance` WHERE drugId IN ('",
+                        trimws(paste(drug$id, sep = "','")),
+                        "')", sep = '')
+substance <- dbGetQuery(con, substanceQuery)
